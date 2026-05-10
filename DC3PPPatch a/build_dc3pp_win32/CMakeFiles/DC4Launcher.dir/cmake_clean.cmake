38e67d59e0257d4a75fe219946f24f212fe18050
SakuraSymphonyReTranslation <akundummybuatan001@gmail.com>
2026-05-10 19:02:20 +0700

    Add remaining build_dc3pp_win32 artifacts

---
+++ DC3PPPatch a/build_dc3pp_win32/CMakeFiles/DC4Launcher.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DC4Launcher.dir/src/launcher.cpp.obj"
+  "CMakeFiles/DC4Launcher.dir/src/launcher.cpp.obj.d"
+  "CMakeFiles/DC4Launcher.dir/src/launcher.rc.res"
+  "DC3PPLauncher.exe"
+  "DC3PPLauncher.exe.manifest"
+  "DC3PPLauncher.lib"
+  "DC3PPLauncher.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX RC)
+  include(CMakeFiles/DC4Launcher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ DC3PPPatch a/build_dc3pp_win32/CMakeFiles/DC4Patch.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DC4Patch.dir/src/dc4patch.cpp.obj"
+  "CMakeFiles/DC4Patch.dir/src/dc4patch.cpp.obj.d"
+  "DC3PPPatch.dll"
+  "DC3PPPatch.dll.manifest"
+  "DC3PPPatch.lib"
+  "DC3PPPatch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DC4Patch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
